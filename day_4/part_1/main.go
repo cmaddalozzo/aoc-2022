16c4ec8f4ffd4bfecdafbf4b251e72b35188d535
Curtis Maddalozzo <curtis@maddalozzo.ca>
2022-12-05 08:57:38 -0500

    Add day 4

---
+++ day_4/part_1/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type assignment struct {
+	start int
+	end   int
+}
+
+func (a assignment) Equals(b assignment) bool {
+	return a.start == b.start && a.end == b.end
+}
+
+func (a assignment) Contains(b assignment) bool {
+	if a.Equals(b) {
+		return true
+	}
+	return a.start <= b.start && a.end >= b.end
+}
+
+func (a assignment) OverlapsWith(b assignment) bool {
+	if a.start >= b.start {
+		return b.end >= a.start
+	}
+	if b.start >= a.start {
+		return a.end >= b.start
+	}
+	return false
+}
+
+func buildAssignment(part string) *assignment {
+	a := assignment{}
+	bounds := strings.Split(part, "-")
+	a.start, _ = strconv.Atoi(bounds[0])
+	a.end, _ = strconv.Atoi(bounds[1])
+	return &a
+}
+
+func main() {
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	numFullyContained := 0
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ",")
+		first := buildAssignment(parts[0])
+		second := buildAssignment(parts[1])
+		fmt.Printf("%d -> %d, %d -> %d\n", first.start, first.end, second.start, second.end)
+		if first.Contains(*second) {
+			numFullyContained += 1
+			fmt.Printf("\t%s contains %s\n", parts[0], parts[1])
+		} else if second.Contains(*first) {
+			numFullyContained += 1
+			fmt.Printf("\t%s contains %s\n", parts[1], parts[0])
+		}
+	}
+	fmt.Printf("Fully contained count: %d\n", numFullyContained)
+}
