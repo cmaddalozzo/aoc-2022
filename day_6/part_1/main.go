fd772f4d36cadfaa320fa2227d085443c59a6a7b
Curtis Maddalozzo <curtis@tractable.ai>
2022-12-06 11:13:38 -0500

    Add day 6

---
+++ day_6/part_1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	_ "strconv"
+	_ "strings"
+)
+
+func findPacketMarker(message string) int {
+	marker_len := 4
+	char_positions := map[rune]int{}
+	unique_start := 0
+	for i, c := range message {
+		pos, exists := char_positions[c]
+		if exists && pos >= unique_start {
+			unique_start = pos + 1
+		}
+		char_positions[c] = i
+		if i-unique_start == marker_len {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	for scanner.Scan() {
+		marker := findPacketMarker(scanner.Text())
+		fmt.Printf("Packet marker: %d\n", marker)
+	}
+}
